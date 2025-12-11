b2081d84e0ef536831ea9e836b9b37dae8873365
Sohyung Kwak <sohyung@Sohyungui-MacBookAir.local>
2025-12-11 15:06:53 +0900

    Weather Route 수정

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 const Intro = () => {
+  const navigate = useNavigate();
   const goToWeather = () => {
     navigate("/Weather");
   }
