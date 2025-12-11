b687988ff1431a8e7e93a9a9a030e3229edd575c
Sohyung Kwak <sohyung@Sohyungui-MacBookAir.local>
2025-12-11 14:51:07 +0900

    productDetail 수정

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-
 import NavigationBar from '../componet/ProductNavigation'
-import { products } from '../../db.json'; // ✅ 상품 리스트 불러오기 (경로는 네 구조에 맞게)
-
 
 const ProductDetail = () => {
-  const params = useParams();
-  // console.log(params) //현재 아이디 키 값 나옴
-    
-  const {id} = useParams();
-  const item = products.find(p => String(p.id) === String(id)); // id로 상품 찾기
+  let {id} = useParams();
+  const [product, setProduct] = useState(null);
 
-  console.log(item);
+  const getProductDetail = async () => {
+    let url = `http://localhost:4000/products/${id}`
+    let response = await fetch(url);
+    let data = await response.json();
+    // console.log(data);
+    setProduct(data)
+  }
+  useEffect(() => {
+    getProductDetail();
+  }, [])
 
   return (
     <div className='shop-wrap'>
@@ -20,19 +23,19 @@ const ProductDetail = () => {
       <div className='product-wrap'>
         <div className='product-detail'>
           <div className='product-img'>
-            <img src={item.img}></img>
+            <img src={product?.img}></img>
           </div>
           <div className='product-info'>
-            <p className='title'>{item.title}</p>
-            <p className='price'>{item.price}</p>
+            <p className='title'>{product?.title}</p>
+            <p className='price'>{product?.price}</p>
             <p className='chips'>
-              {item?.choice && <span className="chip">CHOICE</span>}
-              {item?.new && <span className="chip">NEW</span>}
+              {product?.choice && <span className="chip">CHOICE</span>}
+              {product?.new && <span className="chip">NEW</span>}
             </p>
             <div className='select-box'>
               <select>
                 <option value="0">선택해주세요</option>
-                {item.size.map((option, index) => (
+                {product?.size.map((option, index) => (
                   <option value={option} key={index}>{option}</option>
                 ))}
               </select>
